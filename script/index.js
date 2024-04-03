e2510368229960132df9455cf4b1b5cbf9ab106a
LouisDamay <112948472+LouisDamay@users.noreply.github.com>
2024-04-03 15:32:32 +0200

    page fading

--- script/index.js
+++ script/index.js
@@ -1,14 +1,10 @@
-addEventListener("scroll", (event) => {alert("?")});
-
-onscroll = (event) => {
-    alert("scroll");
+// script.js
+document.addEventListener("scroll", (event) => {
     let overlay = document.getElementById('splashscreen');
-    if (window.scrollY > 50 ) { // Ajustez ce nombre selon vos besoins
+    if (window.scrollY > 1) { // Ajustez ce nombre selon vos besoins
         // Pour faire disparaître graduellement
         overlay.style.opacity = '0';
         setTimeout(function() { overlay.style.display = 'none'; }, 600); // Ajustez le timing si nécessaire
 
-        // OU, pour la supprimer directement
-        overlay.remove();
     }
-};
+});

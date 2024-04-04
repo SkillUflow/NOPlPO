24580afd2682a4e6ece4bae90b5b63da8d5e585f
LouisDamay <112948472+LouisDamay@users.noreply.github.com>
2024-04-04 16:08:47 +0200

    Sync LRC/MP3 V2
    Il n'y a plus qu'à en somme. Je ne vois plus comment avancer là-dessus.

--- louis/library/script.js
+++ louis/library/script.js
@@ -1,35 +1,18 @@
-// Récuperer les deux fichiers côté client (à patch) ------------------------------------------
-// Ben ouais, il faudrait les récupérer côté serveur
+var subtitles = [];
 
-document.getElementById('fileInputLrc').addEventListener('change', function(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var content = e.target.result;
-    parseLrcFile(content);
-  };
-  reader.readAsText(file);
-});
+function parseTimeStamp(timeStamp) {
+  // Convertit le timestamp HH:MM:SS des fichiers LRC en secondes
+  var parts = timeStamp.split(':');
+  var minutes = parseInt(parts[0], 10);
+  var seconds = parseFloat(parts[1]);
+  return minutes * 60 + seconds;
+}
 
-document.getElementById('fileInputAudio').addEventListener('change', function(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var url = URL.createObjectURL(file);
-  var audioPlayer = document.getElementById('audioPlayer');
-  audioPlayer.src = url;
-  audioPlayer.play(); // Commence la lecture dès que l'audio est chargé
-});
+async function loadLRC(link) {
+  const reponse = await fetch(link);
+  const lrcContent = await reponse.text();
 
-// -----------------------------------------------------------------------------------------
-
-function parseLrcFile(lrcContent) {
   var lines = lrcContent.split('\n'); // Split par lignes
-  var subtitles = [];
 
   for (let i = 0; i < lines.length; i++) {
     var line = lines[i].trim();
@@ -42,22 +25,28 @@ function parseLrcFile(lrcContent) {
       }
     }
   }
-  
-  var audioPlayer = document.getElementById('audioPlayer');
-  audioPlayer.addEventListener('timeupdate', function() {
-    var currentTime = audioPlayer.currentTime; // Ouais c'est un méga truc de bourrin
-    subtitles.forEach(subtitle => {
-      if (Math.abs(currentTime - subtitle.time) < 0.5) { // Ajustez ce seuil au besoin
-        console.log(subtitle.text); // Affiche le sous-titre proche du temps actuel
-      }
-    });
-  });
 }
 
-function parseTimeStamp(timeStamp) {
-  // Convertit le timestamp HH:MM:SS des fichiers LRC en secondes
-  var parts = timeStamp.split(':');
-  var minutes = parseInt(parts[0], 10);
-  var seconds = parseFloat(parts[1]);
-  return minutes * 60 + seconds;
+async function loadMP3(link){
+  const reponse = await fetch(link);
+  const url = reponse.url;
+
+  var audioPlayer = document.getElementById('audioPlayer');
+  audioPlayer.src = url;
+
+  audioPlayer.play(); 
+  // Non-supporté par Firefox. La tuile !
 }
+
+var current_song = "Orelsan - Basique"
+var LRCfile = loadLRC(("/library/lyrics/"+current_song+".lrc"))
+var MP3file = loadMP3(("/library/songs/"+current_song+".mp3"))
+  
+audioPlayer.addEventListener('timeupdate', function() {
+  var currentTime = audioPlayer.currentTime; // Ouais c'est un méga truc de bourrin
+  subtitles.forEach(subtitle => {
+    if (Math.abs(currentTime - subtitle.time) < 0.5) { // Ajustez ce seuil au besoin
+      console.log(subtitle.text); // Affiche le sous-titre proche du temps actuel
+    }
+  });
+});

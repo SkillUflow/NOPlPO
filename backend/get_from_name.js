e4dbc48e97b8b2a3fceec0a5398215d18a6dd3b9
rmatarese <robin.matarese@student.junia.com>
2024-04-10 18:28:59 +0200

    Update get_from_name.js

--- backend/get_from_name.js
+++ backend/get_from_name.js
@@ -3,25 +3,19 @@ const fs = require('fs');
 const util = require("util");
 
 async function getFromName(name){
-  let db = new sqlite3.Database('./songs.db');
-  
-  // Promisify the db.all() method
-  const dbAll = util.promisify(db.all.bind(db));
 
-  let sql = "SELECT * FROM 'songs' WHERE name = ' Je te donne'";
-  
-  try {
-    // Use await with the promisified dbAll
-    const rows = await dbAll(sql, [name]);
-    console.log("NAMES_BOUCLE = ", rows);
-    return rows;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  } finally {
-    // Make sure to close the database connection
-    db.close();
-  }
+  let db = new sqlite3.Database('./songs.db');
+  let sql = "SELECT lyrics FROM 'songs' WHERE name = '"+name+"'";
+  let result= db.get(sql, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row);
+    return row;
+  });
+ 
 }
+
+getFromName(' Je te donne');
 
 module.exports = {getFromName};

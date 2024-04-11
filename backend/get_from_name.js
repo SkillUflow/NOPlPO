90297493d2e23c947ff5bb6731c28ae8189ad4ba
AymericDrls <craftandclashyt@gmail.com>
2024-04-11 09:48:38 +0200

    la requette fonctionne

--- backend/get_from_name.js
+++ backend/get_from_name.js
@@ -2,29 +2,26 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const util = require("util");
 
-async function getFromName(name){
-
+async function getFromName(name) {
   let db = new sqlite3.Database('./songs.db');
-  let sql = "SELECT lyrics FROM 'songs' WHERE name = '"+name+"'";
-  let result = await db.get(sql, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("1. ROW :");
-    console.log(row);
-    console.log("-----\n\n");
-    return row;
-  });
 
-  console.log("2. RESULT :"); 
-  console.log(result);
-  console.log("-----\n\n");
-  return result;
- 
+  let sql = "SELECT lyrics FROM 'songs' WHERE name = ?";
+
+  return new Promise((resolve, reject) => {
+    db.get(sql, [name.name], (err, row) => {
+      db.close();
+      
+      if (err) {
+        console.error(err.message);
+        reject(err); 
+      } else {
+        console.log("ROW :", row);
+        resolve(row); 
+      }
+    });
+  });
 }
 
-// Testing
-// let t = getFromName(' Je te donne');
-// console.log(t);
+
 
 module.exports = {getFromName};

3c977466736cea9abe597c7983e139cad62d499d
Romain <romain.ryckebusch@student.junia.com>
2024-04-11 19:59:52 +0200

    Added redirect for root URL

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('../frontend')); // Serve the frontend folder (thus, put every static file in this folder)
 
+app.get('/', (req, res) => {
+  res.redirect('/select.html');
+});
+
 app.post('/calculate-score', (req, res) => {
     const {typed_lyrics, correct_lyrics} = req.body;
     const score = compareLyrics(correct_lyrics, typed_lyrics);

5c27f3e93fa39119c99b695fa8766d88e9fd98b6
rmatarese <robin.matarese@student.junia.com>
2024-04-12 21:39:20 +0200

    J'ai fix des trucs et des machins

--- backend/addNewSong.js
+++ backend/addNewSong.js
@@ -7,41 +7,16 @@ const path = require('path');
 
 
 function addFile(fileText,fileMusic, callback) {
-
-
-
-    //on vérifie que les fichiers ont le meme nom
-    if(fileText.originalname.split('.')[0] !== fileMusic.originalname.split('.')[0]){
-        console.error('Files do not have the same name');
-        if(callback) callback('Files do not have the same name'); 
-        return;
-    }
-
-    //on vérifie que le fichier est bien un fichier mp3
-    else if (fileMusic.mimetype !== 'audio/mpeg') {
-        console.error('File is not an mp3 fileMusic');
-        if(callback) callback('File is not an mp3 fileMusic'); 
-        return;
-    }
-    //on ajoute le fichier dans le répositoire
-    else fs.copyFile(fileMusic.path, '../frontend/assets/mp3_library' + fileMusic.originalname, (err) => {
-        console.log("meh")
-        if (err) { 
-            console.error(err); 
-            if(callback) callback(err); 
-            return; 
-        } 
-        console.log('File Music saved successfully!'); 
-    });
-
-    //avant d'enregistrer le fichier, on vérifie qu'il est en .txt ou en .lrc
+    //avant d'enregistrer le fichier, on vérifie qu'il est en .txt ou en .lrc et qu'ils ont le meme nom
 
     const fileContent = fs.readFileSync(fileText.path, 'utf8');
+    
     if (fileText.originalname.split('.').pop() !== 'lrc' && fileText.originalname.split('.').pop() !== 'txt') {
         console.error('File is not a .lrc or .txt fileText');
         if(callback) callback('File is not a .lrc or .txt fileText'); 
         return;
     }
+    
     //on vérifie que le fichier ne contient pas de code malveillant
     else if (fileContent.includes('<script>')) {
         console.error('File contains malicious code');
@@ -59,13 +34,15 @@ function addFile(fileText,fileMusic, callback) {
     //enfin, on vérifie que le fichier contient au moins un nom d'artiste, un titre de chanson et des paroles
     else if (!fileContent.includes('[ar:') || !fileContent.includes('[ti:') || !fileContent.includes('[00:')) {
         console.error('File does not contain artist, title or lyrics');
+        //on enregistre le nom de la chanson et de l'artiste
+        const artist = fileContent.match(/\[ar:(.*?)\]/)[1];
+        const title = fileContent.match(/\[ti:(.*?)\]/)[1];
         if(callback) callback('File does not contain artist, title or lyrics'); 
         return;
     }
 
     // On enregistre le fichier dans un dossier
     else fs.copyFile(fileText.path, '../lrc_library' + fileText.originalname, (err) => {
-        console.log("mais wtf")
         if (err) { 
             console.error(err); 
             if(callback) callback(err); 
@@ -73,7 +50,23 @@ function addFile(fileText,fileMusic, callback) {
         } 
         console.log('File Text saved successfully!'); 
     });
-    console.log('Coucou');
+
+    //on vérifie que le fichier est bien un fichier mp3
+    if (fileMusic.mimetype !== 'audio/mpeg') {
+        console.error('File is not an mp3 fileMusic');
+        if(callback) callback('File is not an mp3 fileMusic'); 
+        return;
+    }
+    //on ajoute le fichier dans le répositoire
+     else {fs.copyFile(fileMusic.path, `../frontend/assets/mp3_library/${artist} - ${title}.mp3`, (err) => {
+        if (err) { 
+            console.error(err); 
+            if(callback) callback(err); 
+            return; 
+        } 
+        console.log('File Music saved successfully!'); 
+    });}
+
     // bon bah voila, c'est ajouté. On a plus qu'à laisser les admins vérifier que c'est bien une chanson et pas un truc chelou; puis ils n'auront qu'à utiliser create_DB pour mettre la base à jour !
 }
 module.exports = addFile;
